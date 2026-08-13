962371ceb25084fb7413eef2cfd95cd94395b9a4
p01arst0rm <dev@ever3st.com>
2026-08-13 14:56:33 +0000

    finished missing functionality

--- Foundations-7.0/9/9.1.1.ps1
+++ Foundations-7.0/9/9.1.1.ps1
@@ -8,7 +8,6 @@ if (-not $(az account show --output json 2>$null | ConvertFrom-Json)) {
 }
 
 # Obtain Fabric access token
-
 $AccessToken = az account get-access-token `
     --resource "https://api.fabric.microsoft.com" `
     --query accessToken `
@@ -27,7 +26,7 @@ if (-not $AccessToken) {
         -o tsv
 }
 
-
+# Fetch tenant config
 try {
     $Headers = @{Authorization = "Bearer $($AccessToken)"}
     $Uri = "https://api.fabric.microsoft.com/v1/admin/tenantsettings"
@@ -36,4 +35,25 @@ try {
     switch ($_.Exception.Response.StatusCode.value__) {
         403 {Write-Host "** FAIL : Fabric Access is denied. (need Fabric admin?) **"}
     } return
+}
+
+# Audit the config
+$GuestAccess = $TenantSettings.tenantSettings |
+    Where-Object { $_.settingName -eq "AllowGuestUserToAccessSharedContent" }
+
+$Audit = [PSCustomObject]@{
+    SettingName           = $GuestAccess.settingName
+    Enabled               = $GuestAccess.enabled
+    EnabledSecurityGroups = ($GuestAccess.enabledSecurityGroups | ForEach-Object { $_.name }) -join ", "
+}
+
+$Audit | Format-List
+
+if ($GuestAccess.enabled -eq $false) {
+    Write-Host "** PASS : Guest Access is disabled. **"
+}
+elseif ($GuestAccess.enabled -eq $true -and @($GuestAccess.enabledSecurityGroups).Count -gt 0) {
+    Write-Host "** PASS : Guest User access is restricted.**"
+} else {
+    Write-Host "** FAIL : Guest User access is not restricted. **"
 }
