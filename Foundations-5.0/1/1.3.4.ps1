483c7c8c1132889ec6cf6f42efc4e67696ed6669
p01arst0rm <dev@ever3st.com>
2026-08-20 16:08:16 +0000

    improved repoorting for 1.3.4

--- Foundations-5.0/1/1.3.4.ps1
+++ Foundations-5.0/1/1.3.4.ps1
@@ -4,13 +4,28 @@
 
 Connect-MgGraph -Scopes "OrgSettings-AppsAndServices.Read.All" -NoWelcome
 
+$AuditState = "PASS"
 $Uri = "https://graph.microsoft.com/beta/admin/appsAndServices/settings"
 $AppsAndServicesStatus = Invoke-MgGraphRequest -Uri $Uri
 
-if ($AppsAndServicesStatus.isOfficeStoreEnabled  -or $AppsAndServicesStatus.isAppAndServicesTrialEnabled) {
-    Write-Host "** FAIL **"
+Write-Host "isOfficeStoreEnabled: $($AppsAndServicesStatus.isOfficeStoreEnabled)"
+if ($AppsAndServicesStatus.isOfficeStoreEnabled -eq $null) {
+    Write-Host "** ERROR : Unable to retrieve User owned apps and services settings. **"
+    return
+} if ($AppsAndServicesStatus.isOfficeStoreEnabled -eq $true) {
+    $AuditState = "FAIL"
 } else {
     Write-Host "** PASS **"
 }
 
-$AppsAndServicesStatus | Format-Table isOfficeStoreEnabled, isAppAndServicesTrialEnabled -AutoSize
+Write-Host "isAppAndServicesTrialEnabled: $($AppsAndServicesStatus.isAppAndServicesTrialEnabled)"
+if ($AppsAndServicesStatus.isAppAndServicesTrialEnabled -eq $null) {
+    Write-Host "** ERROR : Unable to retrieve User owned apps and services settings. **"
+    return
+} elseif ($AppsAndServicesStatus.isAppAndServicesTrialEnabled -eq $true) {
+    $AuditState = "FAIL"
+} else {
+    Write-Host "** PASS **"
+}
+
+
